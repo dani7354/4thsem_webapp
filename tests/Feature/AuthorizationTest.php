f1fd6dcc582aae05f4f0e0b628b9eaf1eb0816e1
dani7354 <dani7354@edu.eal.dk>
2019-03-11 14:40:36 +0100

    AuthorizationTests added

---
+++ tests/Feature/AuthorizationTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Role;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthorizationTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+
+        // testdata
+        $this->testCourse = array(
+            'name' => 'Madkursus',
+            'description' => 'Lær at lave italiensk mad',
+            'start' => '2019-04-10T08:00',
+            'end' => '2019-04-10T20:00',
+            'location' => 'Køkken 2',
+        );
+
+        parent::__construct($name, $data, $dataName);
+    }
+
+    private function addTokenAuth($header="")
+    {
+        $this->withHeaders(
+            [
+                'Authorization' => 'Bearer ' . $header,
+                'Accept' => 'application/json',
+            ]);
+    }
+
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+    public function testAdminCanCreateCourse(){
+        $user = User::find(2);
+        $this->addTokenAuth($user->api_token);
+
+        $body = $this->testCourse;
+        $response = $this->post('/api/courses', $body);
+        $response->assertStatus(201);
+   }
+
+   public function testForbiddenEmployeeCannotCreateCourse()
+   {
+       $user = User::find(1);
+       $this->addTokenAuth($user->api_token);
+
+       $body = $this->testCourse;
+       $response = $this->post('/api/courses', $body);
+       $response->assertStatus(403);
+
+   }
+
+   public function testUnauthorizedRequestCannotCreateCourse401()
+   {
+       $this->addTokenAuth();
+       $body = $this->testCourse;
+       $response = $this->post('/api/courses', $body);
+       $response->assertStatus(401);
+   }
+}
