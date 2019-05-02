10b700af2fc933bf089255590cc9d4b89667151b
dani7354 <dani7354@edu.eal.dk>
2019-05-02 09:52:48 +0200

    - collection resource for articles

---
+++ app/Http/Resources/ArticleCollection.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ArticleCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            $this->collection->transform(function ($article) {
+                return [
+                    'id' => $article->id,
+                    'title' => $article->title,
+                    'content' => $article->content,
+                    'date_created' => $article->date_created,
+                    'author' => $article->author,
+                    'tags' => $article->tags
+                ];
+
+            })
+
+        ];
+
+    }
+}
