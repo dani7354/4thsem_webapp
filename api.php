4a971c149da6b8d0bb093e1e1daa15b1f3b8ffbc
dani7354 <dani7354@edu.eal.dk>
2019-05-15 13:38:40 +0200

    auth

--- api.php
+++ api.php
@@ -28,7 +28,7 @@ Route::group(['prefix' => 'courses'], function(){
     Route::get('{course}', 'API\CoursesController@show');
 
 
-    //   Route::middleware('auth:api')->group(function () {
+       Route::middleware('auth:api')->group(function () {
 
         Route::post('', 'API\CoursesController@store');
         Route::put('{course}', 'API\CoursesController@update');
@@ -38,7 +38,7 @@ Route::group(['prefix' => 'courses'], function(){
         Route::post('{course}/participants', 'API\CoursesController@participate');
         Route::delete('{course}/participants', 'API\CoursesController@cancel');
 
-    //  });
+      });
 
 });
 
@@ -51,13 +51,13 @@ Route::group(['prefix' => 'articles'], function(){
     Route::get('{article}', 'API\ArticlesController@show');
     Route::get('tag/{tag}', 'API\ArticlesController@get_by_tag');
 
-    //   Route::middleware('auth:api')->group(function (){
+       Route::middleware('auth:api')->group(function (){
         Route::post('', 'API\ArticlesController@store');
         Route::put('{article}', 'API\ArticlesController@update');
         Route::delete('{article}', 'API\ArticlesController@destroy');
 
 
-    // });
+     });
 });
 
 // deadlines
@@ -68,11 +68,11 @@ Route::group(['prefix' => 'deadlines'], function(){
     Route::get('{deadline}', 'API\DeadlinesController@show');
     Route::get('/date/{date}', 'API\DeadlinesController@get_by_date');
 
-    //  Route::middleware('auth:api')->group(function (){
+      Route::middleware('auth:api')->group(function (){
         Route::put('{deadline}', 'API\DeadlinesController@update');
         Route::delete('{deadline}', 'API\DeadlinesController@destroy');
         Route::post('', 'API\DeadlinesController@store');
-    //  });
+      });
 });
 
 // meetings
@@ -82,12 +82,12 @@ Route::group(['prefix' => 'meetings'], function () {
     Route::get('', 'API\MeetingsController@index');
     Route::get('{meeting}', 'API\MeetingsController@show');
 
-    //  Route::middleware('auth:api')->group(function () {
+      Route::middleware('auth:api')->group(function () {
     Route::get('/date/{date}', 'API\MeetingsController@get_by_date');
         Route::put('{meeting}', 'API\MeetingsController@update');
         Route::delete('{meeting}', 'API\MeetingsController@destroy');
         Route::post('', 'API\MeetingsController@store');
-    //  });
+      });
 });
 
 // tokens
@@ -97,4 +97,4 @@ Route::middleware('auth:api')->group(function () {
 
 });
 
-//Route::post('login', 'API\APITokenController@login');
+Route::post('login', 'API\APITokenController@login');
